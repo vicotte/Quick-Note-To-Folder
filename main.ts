118ca50fab296c2c179a050479fa70d9320ba151
victor dugue <vicdugue@gmail.com>
2023-10-06 23:22:28 +0200

    init

--- main.ts
+++ main.ts
@@ -1,14 +1,28 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
-
-// Remember to rename these classes and interfaces!
+import {
+	Plugin,
+	Editor,
+	PluginSettingTab,
+	Setting,
+	App,
+	Modal,
+	TFolder,
+	Notice,
+} from "obsidian";
 
 interface MyPluginSettings {
-	mySetting: string;
+	folders: [
+		{
+			folderName: string;
+		}
+	];
 }
-
 const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+	folders: [
+		{
+			folderName: "default",
+		},
+	],
+};
 
 export default class MyPlugin extends Plugin {
 	settings: MyPluginSettings;
@@ -16,93 +30,70 @@ export default class MyPlugin extends Plugin {
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		for (let i = 0; i < this.settings.folders.length; i++) {
+			this.addCommand({
+				id:
+					"create-note-from-highlight-" +
+					this.settings.folders[i].folderName,
+				name:
+					"Create Note from Highlight - " +
+					this.settings.folders[i].folderName,
+				editorCallback: async (editor: Editor) => {
+					const selectedText = editor.getSelection();
+					if (!selectedText) return;
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+					const newNoteName = selectedText.trim();
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
+					const folder = this.app.vault.getAbstractFileByPath(
+						this.settings.folders[i].folderName
+					);
+
+					if (newNoteName) {
+						new Notice(`Hello!`);
+						const folderPath = this.settings.folders[i].folderName;
+						const newNotePath = `${folderPath}/${newNoteName}.md`;
+
+						if (!(folder instanceof TFolder)) {
+							await this.app.vault.createFolder(folderPath);
+						}
+						await this.app.vault.create(newNotePath, "");
+						const wikilink = `[[${newNoteName}]]`;
+						editor.replaceSelection(wikilink);
 					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
+				},
+			});
+		}
 		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
+	onunload() {}
 
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
+
+	async loadSettings() {
+		this.settings = Object.assign(
+			{},
+			DEFAULT_SETTINGS,
+			await this.loadData()
+		);
+	}
 }
 
 class SampleModal extends Modal {
+	plugin: MyPlugin;
 	constructor(app: App) {
 		super(app);
 	}
 
 	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
+		const { contentEl } = this;
+		contentEl.setText(this.plugin.settings.folders[0].folderName);
 	}
 
 	onClose() {
-		const {contentEl} = this;
+		const { contentEl } = this;
 		contentEl.empty();
 	}
 }
@@ -116,19 +107,44 @@ class SampleSettingTab extends PluginSettingTab {
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+			.setName("Folder Shortcuts")
+			.setDesc("Define shortcuts for writing to different folders")
+			.addButton((button) =>
+				button.setButtonText("Add Shortcut").onClick(() => {
+					this.plugin.settings.folders.push({
+						folderName: "",
+					});
+					this.plugin.saveSettings();
+					this.display();
+				})
+			);
+
+		for (let i = 0; i < this.plugin.settings.folders.length; i++) {
+			const folder = this.plugin.settings.folders[i];
+			new Setting(containerEl)
+				.setName("Folder Shortcuts")
+				.setDesc("Define shortcuts for writing to different folders")
+				.addText((text) =>
+					text
+						.setPlaceholder("Enter your secret")
+						.setValue(folder.folderName)
+						.onChange(async (value) => {
+							folder.folderName = value;
+							await this.plugin.saveSettings();
+						})
+				)
+				.addButton((button) =>
+					button.setButtonText("Remove").onClick(() => {
+						this.plugin.settings.folders.splice(i, 1);
+						this.plugin.saveSettings();
+						this.display();
+					})
+				);
+		}
 	}
 }
